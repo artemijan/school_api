6361fa1c64978b27991b96738bfbc730ce7b71d0
artem <lineartem@gmail.com>
2016-03-22 17:24:42 +0200

    Blueprints auto registration

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import os
 from flask import Flask
 import common.db as db
-from routes.auth import auth
-from routes.users import users
+import pkgutil
+import routes
 from common.app_config import Config
+
 # initialization
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getattr(Config, 'SECRET_KEY', '!@#$%^*DFHF!@$$())FHF!@#$@#%$$%')
 
-app.register_blueprint(auth)
-app.register_blueprint(users)
+"""
+Auto registration all routes through Blueprints
+"""
+package_path = os.path.dirname(routes.__file__)
+for name in pkgutil.iter_modules([package_path]):
+    moduleName = name[1]
+    module = __import__('routes.' + moduleName)
+    app.register_blueprint(getattr(getattr(module, moduleName), moduleName))
 
 if __name__ == '__main__':
     db.init_db()
