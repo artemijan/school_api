5fdef2003818afccdfb727498f2099a1f346b224
artem <lineartem@gmail.com>
2016-03-30 15:37:51 +0300

    remove unused code

--- common/app_config.py
+++ common/app_config.py
@@ -5,14 +5,3 @@ class Config(object):
     SESSION_TOKEN_DURATION = 1200
 
 
-class ProductionConfig(Config):
-    DATABASE_URI = 'mysql://user@localhost/foo'
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
-
