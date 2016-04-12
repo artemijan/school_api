0f15a538110dab7311230632406788c5b628ec84
artem <lineartem@gmail.com>
2016-04-12 15:12:00 +0300

    extend validators

--- common/validators.py
+++ common/validators.py
@@ -12,5 +12,45 @@ def v_not_null(*args, **kwargs):
     value = args[1]
     key = args[0]
     if value is None:
-        raise ValueError("Not null value error of {0}".format(key))
+        raise ValueError("Not null value error for key: {0}".format(key))
+    return value
+
+
+def v_positive(*args, **kwargs):
+    value = args[1]
+    key = args[0]
+    if value <= 0:
+        raise ValueError("Positive value error for key: {0}".format(key))
+    return value
+
+
+def v_negative(*args, **kwargs):
+    value = args[1]
+    key = args[0]
+    if value >= 0:
+        raise ValueError("Negative value error for key: {0}".format(key))
+    return value
+
+
+def v_max_length(*args, **kwargs):
+    value = args[1]
+    key = args[0]
+    max_length = kwargs.get('max_length', -1)
+    if isinstance(value, str) and max_length > -1:
+        if len(value) > max_length:
+            raise ValueError("Max length value error for key: {0}".format(key))
+    else:
+        raise ValueError("{0} value should be a string.".format(key))
+    return value
+
+
+def v_min_length(*args, **kwargs):
+    value = args[1]
+    key = args[0]
+    min_length = kwargs.get('max_length', -1)
+    if isinstance(value, str) and min_length > -1:
+        if len(value) < min_length:
+            raise ValueError("Min length value error for key: {0}".format(key))
+    else:
+        raise ValueError("{0} value should be a string.".format(key))
     return value
